fix: wrong order of arguments

src/preprocessing.py
@@ -1,17 +1,18 @@
 import os
 import sys
-
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 import yaml
+import torch
+from torch.utils.data import TensorDataset, DataLoader
 
 
 def create_windows(data: np.ndarray, window_size):
     return data[np.arange(window_size) + np.arange(
         data.shape[0] - window_size).reshape(-1, 1)]
 
-def featurize(test, train, window_size):
+def featurize(train, test, window_size):
     """
 
     Parameters
@@ -29,3 +30,8 @@ def featurize(test, train, window_size):
         1]*window_size)
 
     return window_train, window_test
+
+
+def get_dataloader(data, batch_size):
+    tensor_d = torch.Tensor(data)
+    return DataLoader(tensor_d, batch_size=batch_size)